c833789eef9faeb0a982337df221f9fedd2d4707
HarshaLinuxmint <officialytmkbhd@gmail.com>
2025-03-30 01:44:25 +0530

    Batch update

--- Breakwithlabels/Breakwithlabels.java
+++ Breakwithlabels/Breakwithlabels.java
@@ -1,29 +1,32 @@
 public class Breakwithlabels {
- public static void main(String[] args) { 
-        
-        int i =1 ; 
-        outer:while (i<4){
-        
-            int j = 1 ;
-            inner:while(j<6){
-                
-                
-                if ( j==4){
-                    break inner ;
-                    
-                }
-            
-            
-                System.out.println("inside inner " + j + " ");
-            j++;}
-      
-      
-        i++;  
-    System.out.println("Outside inner ");}
-    System.out.println("End of the Loop");
-    System.out.println("OUT OF EVERTHING ");    
-    }
-    
+    public static void main(String[] args) {
 
+        int i = 1;
+
+        outer: while (i < 4) {
+
+            int j = 1;
+            inner: while (j < 6) {
+
+                if (j == 4) {
+                    break inner;
+
+                }
+
+                System.out.println("inside inner " + j + " ");
+                j++;
+            }
+
+            i++;
+            System.out.println("Outside inner ");
+
+            if (i == 2) {
+                break outer;
+            }
+
+        }
+        System.out.println("End of the Loop");
+        System.out.println("OUT OF EVERTHING ");
+    }
 
 }
